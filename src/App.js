b4fad43e939cd2626102abe38d7695833364abe0
beyondr <jeopatto@gmail.com>
2021-08-07 14:19:38 -0500

    Add UI/UX improvements

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { useState } from 'react';
 import { ethers } from 'ethers';
 import Guesser from './artifacts/contracts/Guesser.sol/Guesser.json';
 
-const guesserAddress = '0x9E545E3C0baAB3E08CdfD552C960A1050f373042';
+const guesserAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
 function App() {
     const [guesses, setGuesses] = useState([]);
     const [numBy, setNumByValue] = useState('');
     const [_firstGuess, setFirstGuess] = useState(false);
     const [_gameEnded, setGameEnded] = useState(false);
-    const [numberToGuess, setNumberToGuess] = useState(
-        Math.floor(Math.random() * (100 - 1) + 1)
-    );
+    const [_feedbackMessage, updateFeedbackMessage] = useState('');
+    const _guessNum = Math.floor(Math.random() * (100 - 1) + 1);
+    const [numberToGuess] = useState(_guessNum);
+
+    // const numberToGuess = Math.floor(Math.random() * (100 - 1) + 1);
 
     // Request Metamask access
     async function requestAccount() {
@@ -30,12 +32,22 @@ function App() {
         );
         try {
             const data = await contract.getGuess();
+            console.log(numberToGuess, data);
             setGuesses((arr) => [...arr, data]);
             if (_firstGuess === false) setFirstGuess(true);
-            if (guesses.length === 4) setGameEnded(true);
-            console.log('data:', data);
-            console.log('guesses', guesses);
+            if (guesses.length === 4 || data === numberToGuess)
+                setGameEnded(true);
+            updateFeedbackMessage(
+                data < numberToGuess
+                    ? 'ℹ That guess was too low! 🔽'
+                    : data > numberToGuess
+                    ? 'ℹ That guess was too high! 🔼'
+                    : 'Your guess was right! 🎉'
+            );
         } catch (error) {
+            updateFeedbackMessage(
+                "You can't make that guess... try something else 🧙‍♂️"
+            );
             console.error(error);
         }
     }
@@ -50,6 +62,7 @@ function App() {
 
     // One guess function to rule them all
     async function guessBy(type) {
+        updateFeedbackMessage('');
         if (!numBy) return;
         if (isNaN(parseInt(numBy))) return;
         if (numBy < 1 || numBy > 100) return;
@@ -86,20 +99,22 @@ function App() {
         setFirstGuess(false);
         setGameEnded(false);
         setNumByValue('');
-        setNumberToGuess(Math.floor(Math.random() * (100 - 1) + 1));
+        updateFeedbackMessage('');
+        // setNumberToGuess(Math.floor(Math.random() * (100 - 1) + 1));
     }
 
     return (
-        <div className="App">
-            <header className="App-header px-4">
+        <div className="App flex justify-center items-top h-screen pt-10 text-gray-200">
+            <section className="App-header w-1/3">
+                <h1 className="text-5xl font-bold mb-3">DIS-GUESS-TING</h1>
                 {/* <button
                     className="bg-white border-4 border-gray-800 hover:bg-purple-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 px-8 py-2"
                     onClick={() =>}
                 >
                     +
                 </button> */}
-                <ol className="list-decimal ml-4">
-                    <li>I pick a random number 1-100</li>
+                <ol className="list-decimal ml-4 mb-4">
+                    <li>I've picked a random number 1-100</li>
                     <li>
                         You get 5 tries to guess it by doing math on your
                         guesses
@@ -108,16 +123,22 @@ function App() {
                     <li>
                         Winning by multiplication or division rewards +2 NGT
                     </li> */}
-                    <li>FAILURE EXPOSES THE PLAYER TO SCP-096</li>
+                    <li className="font-bold text-yellow-300">
+                        FAILURE EXPOSES THE PLAYER TO SCP-096
+                    </li>
                 </ol>
-                <h3 className="m-1 font-bold">
-                    Number to match: {numberToGuess}
-                </h3>
-                <h3 className="m-1 font-bold">
-                    Previous Guesses: {guesses.join(' ')}
-                </h3>
+                {guesses.length > 0 && (
+                    <h3 className="m-1 font-bold text-yellow-200">
+                        Previous Guesses: {guesses.join(' — ')}
+                    </h3>
+                )}
+                {_gameEnded && (
+                    <h3 className="m-1 font-bold text-xl">
+                        The number was ✨ {numberToGuess} ✨
+                    </h3>
+                )}
                 <input
-                    className="border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent py-1 px-2"
+                    className="border rounded border-purple-300 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:border-transparent py-4 px-3 w-full text-purple-600 "
                     type="text"
                     value={numBy}
                     disabled={_gameEnded}
@@ -125,43 +146,46 @@ function App() {
                     placeholder="Enter a value"
                     autoFocus
                 />
-                <button
-                    className="bg-purple-500 hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 px-8 py-2 text-white disabled:opacity-60"
-                    disabled={_gameEnded}
-                    onClick={() => guessBy('add')}
-                >
-                    Add (+)
-                </button>
-                <button
-                    className="bg-purple-500 hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 px-8 py-2 text-white disabled:opacity-60"
-                    disabled={!_firstGuess || _gameEnded}
-                    onClick={() => guessBy('subtract')}
-                >
-                    Subtract (−)
-                </button>
-                <button
-                    className="bg-purple-500 hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50 px-8 py-2 text-white disabled:opacity-60"
-                    disabled={!_firstGuess || _gameEnded}
-                    onClick={() => guessBy('multiply')}
-                >
-                    Multiply (×)
-                </button>
-                <button
-                    className="bg-purple-500 hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 px-8 py-2 text-white disabled:opacity-60"
-                    disabled={!_firstGuess || _gameEnded}
-                    onClick={() => guessBy('divide')}
-                >
-                    Divide (÷)
-                </button>
-                {_gameEnded && (
+                <h2 className="my-2">{_feedbackMessage}</h2>
+                <div className="pt-3 flex flex-auto gap-2 w-full">
                     <button
-                        className="bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50 px-8 py-2 text-white disabled:opacity-60"
-                        onClick={() => resetGuess()}
+                        className="bg-purple-500 rounded hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 p-8  text-white disabled:opacity-60"
+                        disabled={_gameEnded}
+                        onClick={() => guessBy('add')}
                     >
-                        Reset ↺
+                        Add (+)
                     </button>
-                )}
-            </header>
+                    <button
+                        className="bg-purple-500 rounded hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 p-8 text-white disabled:opacity-60"
+                        disabled={!_firstGuess || _gameEnded}
+                        onClick={() => guessBy('subtract')}
+                    >
+                        Subtract (−)
+                    </button>
+                    <button
+                        className="bg-purple-500 rounded hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50 p-8 text-white disabled:opacity-60"
+                        disabled={!_firstGuess || _gameEnded}
+                        onClick={() => guessBy('multiply')}
+                    >
+                        Multiply (×)
+                    </button>
+                    <button
+                        className="bg-purple-500 rounded hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 p-8 text-white disabled:opacity-60"
+                        disabled={!_firstGuess || _gameEnded}
+                        onClick={() => guessBy('divide')}
+                    >
+                        Divide (÷)
+                    </button>
+                    {_gameEnded && (
+                        <button
+                            className="bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50 p-8 text-white disabled:opacity-60"
+                            onClick={() => resetGuess()}
+                        >
+                            Reset ↺
+                        </button>
+                    )}
+                </div>
+            </section>
         </div>
     );
 }
